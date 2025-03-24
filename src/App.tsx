352ce301c19cf33bfda335b774f4166d8366c2c7
leandroeffgen8 <leandroeffgen8@gmail.com>
2025-03-24 12:46:27 -0300

    Inicio de projeto

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import './App.css'
+
+function App() {
+    return (
+        <div className='flex'>
+            <h1 className=''>TEMPERATURA</h1>
+        </div>        
+    )
+}
+
+export default App
---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+// tailwind.config.js
+module.exports = {
+    content: [
+      "./index.html",  // Asegure-se que o HTML está sendo reconhecido
+      "./src/**/*.{js,jsx,ts,tsx}",  // Caminho para arquivos JS/JSX/TS/TSX
+    ],
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+  }
+  
