ffc0fa60cdbb997a7e3e751baaf745bdd9b4092f
leandroeffgen8 <leandroeffgen8@gmail.com>
2025-03-25 10:07:37 -0300

    feat(component): criando um componente Umidade

--- src/components/ListResults/Umidade.tsx
+++ src/components/ListResults/Umidade.tsx
@@ -1,7 +1,13 @@
-const Umidade = () => {
+interface UmidadeProps{
+    humidity: number
+}
+
+const Umidade = ({ humidity }: UmidadeProps) => {
+
     return (
         <div className="w-full bg-white/10 border border-white/15 rounded-[5px] mb-3 p-3">
             <h4 className="text-white/80 text-[20px] border-b border-white/15 pb-3">Umidade</h4>
+            <p className="text-[12px] py-3 text-white">A umidade do ar está em <span className="font-bold border-b-2 border-white">{humidity}%</span>, influenciando diretamente na sensação térmica.</p>
         </div>
     )
 }
