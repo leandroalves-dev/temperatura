9220704df8efb76875a46934befa5cd4820cdee7
leandroeffgen8 <leandroeffgen8@gmail.com>
2025-03-25 09:17:54 -0300

    refactor(componente): Ajuste no componente NascerDoSol

--- src/components/ListResults/NascerDoSol.tsx
+++ src/components/ListResults/NascerDoSol.tsx
@@ -7,7 +7,12 @@ const NascerDoSol = ({ sunrise }: NascerDoSolProps) => {
     const timeStampSunset = sunrise; 
     const dateSunset = new Date(timeStampSunset * 1000);
 
-    console.log('NASCER DO SOL', dateSunset.toLocaleString());
+    const resultDate = dateSunset.toLocaleDateString();
+    const resultTime = dateSunset.toLocaleTimeString();
+
+    const [day, month ] = resultDate.split("/");
+    const [hour, seconds] = resultTime.split(":");
+    const formattedTime = `${hour}:${seconds}`; 
 
     return (
         <div className="w-[50%] bg-white/10 border border-white/15 rounded-[5px] p-3">
@@ -15,11 +20,11 @@ const NascerDoSol = ({ sunrise }: NascerDoSolProps) => {
 
             <div className="flex flex-col justify-center items-center py-3.5 mb-3.5 border-b border-white/15">
                 <div className="flex items-center bg-white/10 rounded-[50%] py-5 px-3">
-                    <span className="text-white text-2xl mr-1">25</span>
-                    <span className="text-white text-[12px]">/ 03</span>
+                    <span className="text-white text-2xl mr-1">{day}</span>
+                    <span className="text-white text-[12px]">/ {month}</span>
                 </div>
             </div>
-            <p className="text-[12px] text-zinc-700">Amanhecer: 06:15 hrs</p>        
+            <p className="text-[12px] text-zinc-700">Amanhecer: {formattedTime} hrs</p>        
         </div>
     )
 }
