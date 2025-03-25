dad15f0d847c23ffb67095f66c6e0337f4520cec
leandroeffgen8 <leandroeffgen8@gmail.com>
2025-03-25 10:07:03 -0300

    feat(utils): criando uma função para formatar data e hora

---
+++ src/utils/formatTime.ts
@@ -0,0 +1,16 @@
+export const formatTimestamp = (timestamp: number) => {
+
+    const date = new Date(timestamp * 1000);
+    const resultDate = date.toLocaleDateString();
+    const resultTime = date.toLocaleTimeString();
+
+    const [day, month] = resultDate.split('/');
+    const [hour, seconds] = resultTime.split(':');
+
+    return{
+        formattedDay: `${day}`,
+        formattedMonth: `${month}`,
+        formattedTime: `${hour}:${seconds}`
+    }
+
+}
